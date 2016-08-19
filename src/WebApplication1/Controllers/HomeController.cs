d9ecad81c2a15a01bc238f6554b4ab9531b414a5
toyoda <yt0901shi1017@gmail.com>
2016-08-19 10:41:12 +0900

    誤ったコミットを修正して正常にビルドされるかを確認する

--- src/WebApplication1/Controllers/HomeController.cs
+++ src/WebApplication1/Controllers/HomeController.cs
@@ -15,7 +15,7 @@ namespace WebApplication1.Controllers
 
         public IActionResult About()
         {
-            ViewData["Message"] == "Your application description page.";
+            ViewData["Message"] = "Your application description page.";
 
             return View();
         }
